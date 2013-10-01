281671211888435f1dc96c249d7a375fe4f6ba40
Shannon Wynn <shannonwynn@mac.com>
2013-10-01 18:24:28 -0400

    Initial Commit

---
+++ demo app/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  demo app
+//
+//  Created by Shannon Wynn on 10/1/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ demo app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demo app
+//
+//  Created by Shannon Wynn on 10/1/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
